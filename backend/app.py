e2ab3b762fc26e3e551bd05409b7e118e42d0907
GHOST\imran <imran.vatic@gmail.com>
2016-12-05 22:13:57 -0800

    Cahnged the code to accept cross-origin requests

--- backend/app.py
+++ backend/app.py
@@ -3,11 +3,16 @@ from __future__ import absolute_import
 import json
 import os
 from urlparse import urlparse
+from datetime import timedelta
+from functools import update_wrapper
 
-from flask import Flask, render_template, request, redirect, session,jsonify, make_response
+from flask import Flask, render_template, request, redirect, session,jsonify, make_response,url_for
+from flask.ext.login import LoginManager, login_required, login_user, \
+    logout_user, current_user, UserMixin
 from flask_sslify import SSLify
-from rauth import OAuth2Service
+from flask_oauth import OAuth
 import requests
+from requests.exceptions import HTTPError
 import urllib, urllib2
 
 from model import db
@@ -23,6 +28,122 @@ app.secret_key = os.urandom(24)
 
 sslify = SSLify(app)
 
+GOOGLE_CLIENT_ID = '786919166452-v1h1kp81u4h1bsrhf1fp9eibt8ree1of.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = '_Cbad7k6yXdmSnSDF9NHPt1f'
+REDIRECT_URI = '/gCallback'
+
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
+login_manager.session_protection = "strong"
+
+oauth = OAuth()
+ 
+google = oauth.remote_app('google',
+                          base_url='https://www.google.com/accounts/',
+                          authorize_url='https://accounts.google.com/o/oauth2/auth',
+                          request_token_url=None,
+                          request_token_params={'scope': 'https://www.googleapis.com/auth/userinfo.email',
+                                                'response_type': 'code'},
+                          access_token_url='https://accounts.google.com/o/oauth2/token',
+                          access_token_method='POST',
+                          access_token_params={'grant_type': 'authorization_code'},
+                          consumer_key=GOOGLE_CLIENT_ID,
+                          consumer_secret=GOOGLE_CLIENT_SECRET)
+
+
+def crossdomain(origin=None, methods=None, headers=None,
+                max_age=21600, attach_to_all=True,
+                automatic_options=True):
+    if methods is not None:
+        methods = ', '.join(sorted(x.upper() for x in methods))
+    if headers is not None and not isinstance(headers, basestring):
+        headers = ', '.join(x.upper() for x in headers)
+    if not isinstance(origin, basestring):
+        origin = ', '.join(origin)
+    if isinstance(max_age, timedelta):
+        max_age = max_age.total_seconds()
+
+    def get_methods():
+        if methods is not None:
+            return methods
+
+        options_resp = app.make_default_options_response()
+        return options_resp.headers['allow']
+
+    def decorator(f):
+        def wrapped_function(*args, **kwargs):
+            if automatic_options and request.method == 'OPTIONS':
+                resp = app.make_default_options_response()
+            else:
+                resp = make_response(f(*args, **kwargs))
+            if not attach_to_all and request.method != 'OPTIONS':
+                return resp
+
+            h = resp.headers
+
+            h['Access-Control-Allow-Origin'] = origin
+            h['Access-Control-Allow-Methods'] = get_methods()
+            h['Access-Control-Max-Age'] = str(max_age)
+            if headers is not None:
+                h['Access-Control-Allow-Headers'] = headers
+            return resp
+
+        f.provide_automatic_options = False
+        return update_wrapper(wrapped_function, f)
+    return decorator
+ 
+@app.route('/')
+@login_required
+def index():
+    access_token = session.get('access_token')
+    if access_token is None:
+        return redirect(url_for('login'))
+ 
+    access_token = access_token[0]
+    from urllib2 import Request, urlopen, URLError
+ 
+    headers = {'Authorization': 'OAuth '+access_token}
+    req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
+                  None, headers)
+    try:
+        res = urlopen(req)
+    except URLError, e:
+        if e.code == 401:
+            # Unauthorized - bad token
+            session.pop('access_token', None)
+            return redirect(url_for('login'))
+        return res.read()
+ 
+    return res.read()
+ 
+ 
+@app.route('/login')
+def login():
+    callback=url_for('authorized', _external=True)
+    return google.authorize(callback=callback)
+ 
+ 
+ 
+@app.route(REDIRECT_URI)
+@google.authorized_handler
+def authorized(resp):
+    access_token = resp['access_token']
+    session['access_token'] = access_token, ''
+    return redirect(url_for('index'))
+ 
+ 
+@google.tokengetter
+def get_access_token():
+    return session.get('access_token')
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
 #Location API's
 @app.route('/locations', methods=['GET'])
 def show_addresses():
@@ -31,8 +152,10 @@ def show_addresses():
   return json.dumps(entries)
 	
 @app.route('/locations', methods=['POST'])
+@crossdomain(origin='*')
 def create_address():
     try:
+        print request
         name = request.json['name']
         address = request.json['address']
         city = request.json['city']
@@ -71,6 +194,8 @@ def create_address():
                 return resp
 
 @app.route('/locations/<address_id>')
+@crossdomain(origin='*')
+@login_required
 def show_address(address_id):
     try:
         user = User.query.filter_by(id=address_id).first_or_404()
@@ -81,6 +206,7 @@ def show_address(address_id):
         return resp
 
 @app.route('/locations/<int:address_id>', methods=['PUT'])
+@crossdomain(origin='*')
 def edit_address(address_id):
 	try:
 		user = User.query.get(address_id)
@@ -98,6 +224,7 @@ def edit_address(address_id):
                 return resp
 
 @app.route('/locations/<int:address_id>', methods=['DELETE'])
+@crossdomain(origin='*')
 def delete_address(address_id):
     try:
         db.session.delete(User.query.get(address_id))
